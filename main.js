dae8ac59d0b78e2ef4c3ed92bba53e74130bf314
Adebola <adebolaakomolafe@gmail.com>
2022-06-12 13:25:53 +0100

    Add cancel

--- main.js
+++ main.js
@@ -2,20 +2,34 @@ function openModal() {
   // Dynamically create the modal elements with the document API.
   let container = document.createElement("div");
   let modal = document.createElement("div");
-  let bar = document.createElement("div");
-  let closeButton = document.createElement("button");
+  let text = document.createTextNode("It works");
+  let header = document.createElement("div");
+  let close = document.createElement("button");
+  let footer = document.createElement("footer");
+  let cancel = document.createElement("button");
+
+  // Create the modal close event.
+  const closeModal = () => {
+    container.remove();
+  };
+
   // Add styling classes and text.
   container.classList.add("modal-container");
   modal.classList.add("modal");
-  bar.classList.add("modal-bar");
-  closeButton.classList.add("modal-close-btn");
-  modal.innerText = "It works";
-  // Add a click event on the close button that closes the entire modal component.
-  closeButton.addEventListener("click", () => {
-    container.remove();
-  });
+  header.classList.add("modal-header");
+  close.classList.add("modal-close");
+  footer.classList.add("modal-footer");
+  cancel.classList.add("modal-cancel");
+  cancel.innerText = "Cancel";
+
+  // Add the close event on the close buttons.
+  close.addEventListener("click", closeModal);
+  cancel.addEventListener("click", closeModal);
+
   // Join elements together.
-  bar.append(closeButton), modal.prepend(bar), container.append(modal);
+  header.append(close), footer.append(cancel);
+  modal.append(header, text, footer), container.append(modal);
+
   // Add the created component to the DOM.
   document.body.prepend(container);
 }
